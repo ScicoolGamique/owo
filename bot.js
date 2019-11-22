5dca101393513b452d3918e33275443ebc3f7288
Sophie <29803948+Sophie-Williams@users.noreply.github.com>
2019-11-22 04:57:06 +0000

    Update bot.js
    replaced if/then with switchcase added default message and some comments

--- bot.js
+++ bot.js
@@ -1,23 +1,42 @@
 const discord = require('discord.js');
 const bot = new discord.Client();
-const token = "NjMxOTY5MjYzNDg5NTE1NTMw.XbDKDw.YqEevmWUlpCSc2u1-odCK1nVqrc";
+const token = "NjMxOTY5MjYzNDg5NTE1NTMw.XbDKDw.YqEevmWUlpCSc2u1-odCK1nVqrc"; //O3O insert your bot token here
+
 bot.on('ready', () => { 
-    bot.user.setActivity('hugging');
+	bot.user.setActivity('hugging');
 console.log('hahah');
 })
 
-bot.on('message', msg =>{
-   if( msg.content === 'hi'){
-msg.reply('OwO');}
-if( msg.content === 'you suck'){
-    msg.reply('(╬ ಠ益ಠ)');
-   }
-   if( msg.content === "I'm bored"){
-    msg.reply('talk to me ( ͡° ͜ʖ ͡°)');
-   }
-   if( msg.content === 'I hate u'){
-    msg.reply('ಥ﹏ಥ');}
-    
-    })
+bot.on('message', msg =>
+{
+	switch(msg.content)
+	{
+		case 'hi':
+		{
+			msg.reply('OwO');
+			break;
+		}
+		case 'you suck':
+		{
+			msg.reply('(╬ ಠ益ಠ)-_-');
+			break;
+		}
+		case 'I\'m bored':
+		{
+			msg.reply('talk to me ( ͡° ͜ʖ ͡°)');
+			break;
+		}
+		case 'I hate u':
+		{
+			msg.reply('ಥ﹏ಥ');
+			break;
+		}	
+		default: //default message
+		{
+			msg.reply('OwO');
+			break;
+		}
+	}
+})
 
 bot.login(token);
