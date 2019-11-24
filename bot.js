bf58806e8bdec05fa266814993ade0820aca4cf3
Sophie <29803948+Sophie-Williams@users.noreply.github.com>
2019-11-24 14:01:07 +0000

    Update bot.js
    updated reply check with a much simpler method and removed the case requirement for messages (accepts: hi/Hi/hI)

--- bot.js
+++ bot.js
@@ -1,42 +1,27 @@
 const discord = require('discord.js');
 const bot = new discord.Client();
-const token = "NjMxOTY5MjYzNDg5NTE1NTMw.XbDKDw.YqEevmWUlpCSc2u1-odCK1nVqrc"; //O3O insert your bot token here
-
-bot.on('ready', () => { 
-	bot.user.setActivity('hugging');
-console.log('hahah');
-})
-
-bot.on('message', msg =>
+const token = "INSERT_BOT_TOKEN_HERE"; //insert bot token here
+const messages =
 {
-	switch(msg.content)
+//	'lower case message' : 'reply',
+	'hi' : 'OwO',
+	'you suck' : '(╬ ಠ益ಠ)-_-',
+	'i\'m bored' : 'talk to me ( ͡° ͜ʖ ͡°)',
+	'i hate u' : 'ಥ﹏ಥ',
+};
+
+bot.on('ready', () =>
+{ 
+	bot.user.setActivity('hugging');
+	console.log('hahah');
+});
+
+bot.on("message", msg =>
+{
+	if (reply_msg = messages[msg.content.toLowerCase()])
 	{
-		case 'hi':
-		{
-			msg.reply('OwO');
-			break;
-		}
-		case 'you suck':
-		{
-			msg.reply('(╬ ಠ益ಠ)-_-');
-			break;
-		}
-		case 'I\'m bored':
-		{
-			msg.reply('talk to me ( ͡° ͜ʖ ͡°)');
-			break;
-		}
-		case 'I hate u':
-		{
-			msg.reply('ಥ﹏ಥ');
-			break;
-		}	
-		default: //default message
-		{
-			//msg.reply('testing..');
-			break;
-		}
+		msg.reply(reply_msg);
 	}
-})
+});
 
 bot.login(token);
