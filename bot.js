838a077ee5fc76211aa11ee445419626ee7bc151
Sophie <29803948+Sophie-Williams@users.noreply.github.com>
2019-11-24 14:04:26 +0000

    Update bot.js
    replaced all ' with "

--- bot.js
+++ bot.js
@@ -1,19 +1,19 @@
-const discord = require('discord.js');
+const discord = require("discord.js");
 const bot = new discord.Client();
 const token = "INSERT_BOT_TOKEN_HERE"; //insert bot token here
 const messages =
 {
-//	'lower case message' : 'reply',
-	'hi' : 'OwO',
-	'you suck' : '(╬ ಠ益ಠ)-_-',
-	'i\'m bored' : 'talk to me ( ͡° ͜ʖ ͡°)',
-	'i hate u' : 'ಥ﹏ಥ',
+//	"lower case message" : "reply",
+	"hi" : "OwO",
+	"you suck" : "(╬ ಠ益ಠ)-_-",
+	"i'm bored" : "talk to me ( ͡° ͜ʖ ͡°)",
+	"i hate u" : "ಥ﹏ಥ",
 };
 
-bot.on('ready', () =>
+bot.on("ready", () =>
 { 
-	bot.user.setActivity('hugging');
-	console.log('hahah');
+	bot.user.setActivity("hugging");
+	console.log("hahah");
 });
 
 bot.on("message", msg =>
