af405296d467c97b3094b7adbbe9aa667d799de5
Sophie <29803948+Sophie-Williams@users.noreply.github.com>
2020-06-07 15:36:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const discord = require('discord.js');
+const discord = require("discord.js");
 const bot = new discord.Client();
 const token = "token";
 const messages =
@@ -10,9 +10,9 @@ const messages =
 	"meow" : ":heart_eyes_cat:",
 };
 
-bot.on('guildMemberAdd', member =>
+bot.on("guildMemberAdd", member =>
 {
-	const channel = member.guild.channels.get('719176116123992075');
+	const channel = member.guild.channels.get("719176116123992075");
 	if (!channel)
 	{
 		return;
