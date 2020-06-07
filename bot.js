dea53861e01c2acf82e50009e9952b283c5e947c
Sophie <29803948+Sophie-Williams@users.noreply.github.com>
2020-06-07 15:33:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,27 @@
-const discord = require("discord.js");
+const discord = require('discord.js');
 const bot = new discord.Client();
-const token = "INSERT_BOT_TOKEN_HERE"; //insert bot token here
+const token = "token";
 const messages =
 {
-//	"lower case message" : "reply",
 	"hi" : "OwO",
 	"you suck" : "(╬ ಠ益ಠ)-_-",
 	"i'm bored" : "talk to me ( ͡° ͜ʖ ͡°)",
 	"i hate u" : "ಥ﹏ಥ",
+	"meow" : ":heart_eyes_cat:",
 };
 
+bot.on('guildMemberAdd', member =>
+{
+	const channel = member.guild.channels.get('719176116123992075');
+	if (!channel)
+	{
+		return;
+	}
+	channel.send(`${member} has entered our clique.`);
+});
+
 bot.on("ready", () =>
-{ 
+{
 	bot.user.setActivity("hugging");
 	console.log("hahah");
 });
